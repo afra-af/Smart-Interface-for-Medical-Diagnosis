7d76fa47ab70089b371196c4557e2f35818a3440
AFRA <81957946+afra-af@users.noreply.github.com>
2023-02-06 00:14:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+
+document.getElementById("signup-form").addEventListener("submit",(event)=>{
+  event.preventDefault()
+})
+
+
+
+
+
